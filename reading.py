cfa36fad1fee43f1b542d1c5d19b63a6eafb7c5f
carlosvinnifernandes <carlos24070061@aluno.cesupa.br>
2025-01-07 16:15:51 -0300

    Selecionando area

--- reading.py
+++ reading.py
@@ -1,5 +1,7 @@
 import os
 import pdfplumber
+from pymupdf.extra import page_count
+
 
 def extract_text_from_pdf(file_path):
     """Extrai texto de um PDF."""
@@ -20,6 +22,7 @@ def process_pdf(file_path):
     return {"text": text}
 
 
+
 if __name__ == '__main__':
     # Caminho do arquivo PDF
     pdf_path = "pdf/gabaritodia22024.pdf"
@@ -28,42 +31,14 @@ if __name__ == '__main__':
         result = process_pdf(pdf_path)
         print("Texto extraído:")
         #print(result["text"])
-        contador = 0
-        contador2 = 0
-        gabarito = []
 
-        # # Supondo que `result["text"]` seja o texto de onde as letras estão sendo extraídas
-        for letras in result["text"]:
-        #     contador2 += 1
-        #     if contador2 > 161:  # Ignorar as primeiras 161 letras
-            gabarito.append(letras)
-            print(letras, end="")
-
-        # Converter o gabarito extraído em um formato processável
-        texto = ''.join(gabarito)  # Juntar todas as letras
-        linhas = texto.split("\n")  # Dividir por linhas
-        gabarito_processado = []
-
-        # Processar o texto extraído
-        for linha in linhas:
-            if linha.strip():  # Ignorar linhas vazias
-                partes = linha.split()
-                numero = int(partes[0])  # Número da questão
-                alternativas = partes[1:]  # Alternativas
-                gabarito_processado.append((numero, alternativas))
-
-        gabarito_processado.sort(key=lambda x: x[0])
-
-        resultado = []
-        for i, (numero, alternativas) in enumerate(gabarito_processado):
-            if i < 5:
-                resultado.append(f"{numero} {' '.join(alternativas[:2])}")
-            else:
-                resultado.append(f"{numero} {alternativas[0]}")
-
-        gabarito_ordenado = "\n".join(resultado)
-        print("\nGabarito Final Ordenado:")
-        print(gabarito_ordenado)
+        # Extrair apenas o texto
+        with pdfplumber.open("pdf/2017gabarito.pdf") as pdf:
+            page = pdf.pages[0]
+            area_gabarito = (100, 220, 500, 780)
+            cropped_page = page.within_bbox(area_gabarito)
+            texto_area = cropped_page.extract_text()
+            print(texto_area)
 
 
 
